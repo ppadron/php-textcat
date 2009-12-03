a277e07af6a6603dac390aa646b4b68b9fb104d6
Pedro Padron <ppadron@w3p.com.br>
2009-12-03 03:50:56 -0200

    TextCategorizer::classify() now returns results as an array

--- tests/TextCategorizerTest.php
+++ tests/TextCategorizerTest.php
@@ -5,12 +5,25 @@ require_once "PHPUnit/Framework/TestCase.php";
 class TextCategorizerTest extends PHPUnit_Framework_TestCase
 {
 
+    public function setUp()
+    {
+        $this->textcat = new TextCategorizer("data/conf.txt");
+        chdir(dirname(__FILE__));
+    }
+
     public function testShouldThrowExceptionIfTextIsShorterThan25Chars()
     {
-        $textcat = new TextCategorizer("data/conf.txt");
+        $this->textcat = new TextCategorizer("data/conf.txt");
         $string = "Very short string";
         $this->expectedException = "TextCategorizerException";
-        $result = $textcat->classify($string);
+        $result = $this->textcat->classify($string);
+    }
+
+    public function testShouldReturnResultsAsArray()
+    {
+        $expected = array("english");
+        $result = $this->textcat->classify("This text should be classified as English.");
+        $this->assertEquals($expected, $result);
     }
 
 }
